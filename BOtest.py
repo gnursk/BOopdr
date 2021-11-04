6a7c4dc5f4e622c09d9f4a2786af35128ce2b190
Nick <32829@ma-web.nl>
2021-11-04 10:37:10 +0100

    verhaal update

---
+++ BOtest.py
@@ -0,0 +1,77 @@
+input()
+print ("Hallo, ik ben Nick. Ik ben een nieuwkomer op het mediacollege.")
+print ("Ik heb een paar multiple choice vragen voor jou om mij beter te leren kennen\n\n")
+d1 = input ("Waar woon ik? \nA Amsterdam\nB Lelystad \nC Enkhuizen \nHier typen: " )
+if d1 == "A" or d1 == "a":
+    print ("Nee, ik woon in Enkhuizen, ik woon hier al mijn hele leven")
+    print ("\nDat was helaas niet goed, maar je hebt nogmeer kansen")
+    d2 = input ("In welke maand ben ik geboren? \nA Juni \nB Juli\nC Augustus\nHier typen:")
+    if d2 == "A" or d2 == "a":
+        print ("\nNee, dat is ook niet goed, ik ben geboren in Juli" )
+        print("maar je weet wat ze zeggen, drie keer is scheepsrecht")
+        d3 = input ("hoeveel huisdieren heb ik?\nA 0\nB 1\nC 2\nHier typen: ")
+        if d3 == "A" or d3 == "a":
+            print ("Alweer niet goed, ik had wel wat beter van je verwacht")
+            print ("De laatste vraag, kom op je kan dit(kies niet C)")
+            d4 = input ("Waar ben ik geboren\nA Nederland \nB Weet ik niet \nC Duitsland\nHier typen:")
+            if d4 == "A" or d4 == "a":
+                print ("Helaas, je hebt alles fout")
+                print ("oja, ik ben geboren in Duitsland")
+        if d3 == "B"or d3 == "b":
+                print ("Correct, ik heb op dit moment één huisdier")
+                d10 = input ("Welk platform gebruik ik om te gamen?\nA XBOX\nB PLAYSTATION\nC PC\nHier typen:")
+                if d10 == "A"or d10 == "a":
+                    print ("Ik speel op dit moment op een Playstation 4")
+        if d3 == "C" or d3 == "c":
+            print("Ik heb één huisdier op dit moment")
+            d11 = input ("wat is mijn favoriete game genre?\nA MOBA\nB RPG\nC FPS\nHier typen:")
+            if d11 == "A" or d11 == "a":
+                print ("Mijn favoriete game genre is FPS,maar mijn favoriete game is wel een MOBA")
+    if d2 == "B" or d2 == "b":
+        print ("Dat klopt, ik ben inderdaad in Juli geboren")
+        d5 = input ("Wat is mijn lievelings eten?\nA Pizza\nB Lasagne\nC Ravioli\nHier typen:")
+        if d5 == "A" or d5 == "a":
+            print ("Mijn favoriete eten is lasagne, helaas")
+            d6 = input ("wat is mijn favoriete game genre?\nA MOBA\nB RPG\nC FPS\nHier typen:")
+            if d6 == "A" or d6 == "a":
+                print ("Mijn favoriete game genre is FPS,maar mijn favoriete game is wel een MOBA")
+                d7 = input ("Wat is mijn favoriete kleur?\nA Lila\nB Grijs\nC Oranje\nHier typen:")
+                if d7 == "A" or d7 == "a":
+                    print ("Mijn favoriete kleur is grijs, erg saai")
+        if d5 == "B"or d5 == "b":
+            print ("Mijn lievelings eten is inderdaad lasagne")
+            d12 = input ("In welk land ben ik geboren?\nA Nederland\nB Duitsland\nC België\nHier typen:")
+            if d12 == "A" or d12 == "a":
+                print ("Ik ben geboren in Duitsland")
+        if d5 == "C" or d5 == "c":
+            print("Mijn lievelings eten is Lasagne, maar ravioli vind ik ook erg lekker")
+            d13 = input ("Welke schoenmaat heb ik?\nA 41\nB 42\nC 43\nHier typen:")
+            if d13 == "A" or d13 == "a":
+                print ("Mijn schoenmaat is 43, helaas")
+    if d2 == "C" or d2 == "c":
+        print ("Helaas, ik ben geboren in Juli")
+        d8 = input ("Welke shoenmaat heb ik?\nA 41\nB 42\nC 43\nHier typen:")
+        if d8 == "A" or d8 == "a":
+            print ("ik heb schoenmaat 43")
+            d9 = input ("Hoe lang ben ik?\nA 1,70\nB 1,75\nC 1,80\nHier typen:")
+            if d9 == "A" or d9 == "a":
+                print ("Ik ben 1.75")
+            if d9 == "B" or d9 == "b":
+                print ("Ik ben inderdaad 1.75")
+            if d9 == "C" or d9 == "c":
+                print ("Ik ben 1.75")
+        if d8 == "B" or d8 == "b":
+            print("Mijn schoenmaat is 43")
+            d14 = input ("Watvoor merk telefoon gebruik ik?\nA Apple\nB Samsung\nC Huawei\nHier typen:")
+            if d14 == "A" or d14 == "a":
+                print ("Ik gebruik op dit moment een Samsung")
+        if d8 == "C" or d8 == "c":
+            print ("Ik heb inderdaad schoenmaat 38")
+            d15 = input ("In welk land ben ik geboren?\nA Nederland\nDuitsland\nBelgië\nHier typen:")
+            if d15 == "A" or d15 == "a":
+                print ("Ik ben in duitsland geboren")
+            if d15 == "B" or d15 == "b":
+                print ("Ik ben in duitsland geboren")
+            if d15 == "C" or d15 == "c":
+                print ("Ik ben in duitsland geboren")
+            
---
+++ BOtest2.py
@@ -0,0 +1,223 @@
+input()
+print ("Hallo, ik ben Nick. Ik ben een nieuwkomer op het mediacollege.")
+print ("Ik heb een paar multiple choice vragen voor jou om mij beter te leren kennen\n\n")
+d1 = input ("Waar woon ik? \nA Amsterdam\nB Lelystad \nC Enkhuizen \nHier typen: " )
+if d1 == "A" or d1 == "a":
+    print ("Nee, ik woon in Enkhuizen, ik woon hier al mijn hele leven")
+    print ("\nDat was helaas niet goed, maar je hebt nogmeer kansen")
+    d2 = input ("In welke maand ben ik geboren? \nA Juni \nB Juli\nC Augustus\nHier typen:")
+    if d2 == "A" or d2 == "a":
+        print ("\nNee, dat is ook niet goed, ik ben geboren in Juli" )
+        print("maar je weet wat ze zeggen, drie keer is scheepsrecht")
+        d3 = input ("hoeveel huisdieren heb ik?\nA 0\nB 1\nC 2\nHier typen: ")
+        if d3 == "A" or d3 == "a":
+            print ("Alweer niet goed, ik had wel wat beter van je verwacht")
+            print ("De laatste vraag, kom op je kan dit(kies niet C)")
+            d4 = input ("Waar ben ik geboren\nA Nederland \nB Weet ik niet \nC Duitsland\nHier typen:")
+            if d4 == "A" or d4 == "a":
+                print ("Helaas, je hebt alles fout")
+                print ("oja, ik ben geboren in Duitsland")
+        if d3 == "B"or d3 == "b":
+                print ("Correct, ik heb op dit moment één huisdier")
+                d10 = input ("Welk platform gebruik ik om te gamen?\nA XBOX\nB PLAYSTATION\nC PC\nHier typen:")
+                if d10 == "A"or d10 == "a":
+                    print ("Ik speel op dit moment op een Playstation 4")
+        if d3 == "C" or d3 == "c":
+            print("Ik heb één huisdier op dit moment")
+            d11 = input ("wat is mijn favoriete game genre?\nA MOBA\nB RPG\nC FPS\nHier typen:")
+            if d11 == "A" or d11 == "a":
+                print ("Mijn favoriete game genre is FPS,maar mijn favoriete game is wel een MOBA")
+    if d2 == "B" or d2 == "b":
+        print ("Dat klopt, ik ben inderdaad in Juli geboren")
+        d5 = input ("Wat is mijn lievelings eten?\nA Pizza\nB Lasagne\nC Ravioli\nHier typen:")
+        if d5 == "A" or d5 == "a":
+            print ("Mijn favoriete eten is lasagne, helaas")
+            d6 = input ("wat is mijn favoriete game genre?\nA MOBA\nB RPG\nC FPS\nHier typen:")
+            if d6 == "A" or d6 == "a":
+                print ("Mijn favoriete game genre is FPS,maar mijn favoriete game is wel een MOBA")
+                d7 = input ("Wat is mijn favoriete kleur?\nA Lila\nB Grijs\nC Oranje\nHier typen:")
+                if d7 == "A" or d7 == "a":
+                    print ("Mijn favoriete kleur is grijs, erg saai")
+        if d5 == "B"or d5 == "b":
+            print ("Mijn lievelings eten is inderdaad lasagne")
+            d12 = input ("In welk land ben ik geboren?\nA Nederland\nB Duitsland\nC België\nHier typen:")
+            if d12 == "A" or d12 == "a":
+                print ("Ik ben geboren in Duitsland")
+        if d5 == "C" or d5 == "c":
+            print("Mijn lievelings eten is Lasagne, maar ravioli vind ik ook erg lekker")
+            d13 = input ("Welke schoenmaat heb ik?\nA 41\nB 42\nC 43\nHier typen:")
+            if d13 == "A" or d13 == "a":
+                print ("Mijn schoenmaat is 43, helaas")
+    if d2 == "C" or d2 == "c":
+        print ("Helaas, ik ben geboren in Juli")
+        d8 = input ("Welke shoenmaat heb ik?\nA 41\nB 42\nC 43\nHier typen:")
+        if d8 == "A" or d8 == "a":
+            print ("ik heb schoenmaat 43")
+            d9 = input ("Hoe lang ben ik?\nA 1,70\nB 1,75\nC 1,80\nHier typen:")
+            if d9 == "A" or d9 == "a":
+                print ("Ik ben 1.75")
+            if d9 == "B" or d9 == "b":
+                print ("Ik ben inderdaad 1.75")
+            if d9 == "C" or d9 == "c":
+                print ("Ik ben 1.75")
+        if d8 == "B" or d8 == "b":
+            print("Mijn schoenmaat is 43")
+            d14 = input ("Watvoor merk telefoon gebruik ik?\nA Apple\nB Samsung\nC Huawei\nHier typen:")
+            if d14 == "A" or d14 == "a":
+                print ("Ik gebruik op dit moment een Samsung")
+        if d8 == "C" or d8 == "c":
+            print ("Ik heb inderdaad schoenmaat 38")
+            d15 = input ("In welk land ben ik geboren?\nA Nederland\nDuitsland\nBelgië\nHier typen:")
+            if d15 == "A" or d15 == "a":
+                print ("Ik ben in duitsland geboren")
+            if d15 == "B" or d15 == "b":
+                print ("Ik ben in duitsland geboren")
+            if d15 == "C" or d15 == "c":
+                print ("Ik ben in duitsland geboren")
+            input()
+if d1 == "B" or d1 == "b":
+    print ("Nee, ik woon in Enkhuizen, ik woon hier al mijn hele leven")
+    print ("\nDat was helaas niet goed, maar je hebt nogmeer kansen")
+    d16 = input ("In welke maand ben ik geboren? \nA Juni \nB Juli\nC Augustus\nHier typen:")
+    if d16 == "A" or d16 == "a":
+        print ("\nNee, dat is ook niet goed, ik ben geboren in Juli" )
+        print("maar je weet wat ze zeggen, drie keer is scheepsrecht")
+        d17 = input ("hoeveel huisdieren heb ik?\nA 0\nB 1\nC 2\nHier typen: ")
+        if d17 == "A" or d17 == "a":
+            print ("Alweer niet goed, ik had wel wat beter van je verwacht")
+            print ("De laatste vraag, kom op je kan dit(kies niet C)")
+            d18 = input ("Waar ben ik geboren\nA Nederland \nB Weet ik niet \nC Duitsland\nHier typen:")
+            if d18 == "A" or d18 == "a":
+                print ("Helaas, je hebt alles fout")
+                print ("oja, ik ben geboren in Duitsland")
+        if d17 == "B"or d17 == "b":
+                print ("Correct, ik heb op dit moment één huisdier")
+                d24 = input ("Welk platform gebruik ik om te gamen?\nA XBOX\nB PLAYSTATION\nC PC\nHier typen:")
+                if d10 == "A"or d24 == "a":
+                    print ("Ik speel op dit moment op een Playstation 4")
+        if d17 == "C" or d17 == "c":
+            print("Ik heb één huisdier op dit moment")
+            d25 = input ("wat is mijn favoriete game genre?\nA MOBA\nB RPG\nC FPS\nHier typen:")
+            if d25 == "A" or d25 == "a":
+                print ("Mijn favoriete game genre is FPS,maar mijn favoriete game is wel een MOBA")
+    if d16 == "B" or d16 == "b":
+        print ("Dat klopt, ik ben inderdaad in Juli geboren")
+        d19 = input ("Wat is mijn lievelings eten?\nA Pizza\nB Lasagne\nC Ravioli\nHier typen:")
+        if d19 == "A" or d19 == "a":
+            print ("Mijn favoriete eten is lasagne, helaas")
+            d20 = input ("wat is mijn favoriete game genre?\nA MOBA\nB RPG\nC FPS\nHier typen:")
+            if d20 == "A" or d20 == "a":
+                print ("Mijn favoriete game genre is FPS,maar mijn favoriete game is wel een MOBA")
+                d21 = input ("Wat is mijn favoriete kleur?\nA Lila\nB Grijs\nC Oranje\nHier typen:")
+                if d21 == "A" or d21 == "a":
+                    print ("Mijn favoriete kleur is grijs, erg saai")
+        if d19 == "B"or d19 == "b":
+            print ("Mijn lievelings eten is inderdaad lasagne")
+            d26 = input ("In welk land ben ik geboren?\nA Nederland\nB Duitsland\nC België\nHier typen:")
+            if d26 == "A" or d26 == "a":
+                print ("Ik ben geboren in Duitsland")
+        if d19 == "C" or d19 == "c":
+            print("Mijn lievelings eten is Lasagne, maar ravioli vind ik ook erg lekker")
+            d27 = input ("Welke schoenmaat heb ik?\nA 41\nB 42\nC 43\nHier typen:")
+            if d27 == "A" or d27 == "a":
+                print ("Mijn schoenmaat is 43, helaas")
+    if d16 == "C" or d16 == "c":
+        print ("Helaas, ik ben geboren in Juli")
+        d22 = input ("Welke shoenmaat heb ik?\nA 41\nB 42\nC 43\nHier typen:")
+        if d22 == "A" or d22 == "a":
+            print ("ik heb schoenmaat 43")
+            d23 = input ("Hoe lang ben ik?\nA 1,70\nB 1,75\nC 1,80\nHier typen:")
+            if d23 == "A" or d23 == "a":
+                print ("Ik ben 1.75")
+            if d23 == "B" or d23 == "b":
+                print ("Ik ben inderdaad 1.75")
+            if d23 == "C" or d23 == "c":
+                print ("Ik ben 1.75")
+        if d22 == "B" or d22 == "b":
+            print("Mijn schoenmaat is 43")
+            d28 = input ("Watvoor merk telefoon gebruik ik?\nA Apple\nB Samsung\nC Huawei\nHier typen:")
+            if d28 == "A" or d28 == "a":
+                print ("Ik gebruik op dit moment een Samsung")
+        if d22 == "C" or d22 == "c":
+            print ("Ik heb inderdaad schoenmaat 38")
+            d29 = input ("In welk land ben ik geboren?\nA Nederland\nDuitsland\nBelgië\nHier typen:")
+            if d29 == "A" or d29 == "a":
+                print ("Ik ben in duitsland geboren")
+            if d29 == "B" or d29 == "b":
+                print ("Ik ben in duitsland geboren")
+            if d29 == "C" or d29 == "c":
+                print ("Ik ben in duitsland geboren")
+            input()
+if d1 == "C" or d1 == "c":
+    print ("Nee, ik woon in Enkhuizen, ik woon hier al mijn hele leven")
+    print ("\nDat was helaas niet goed, maar je hebt nogmeer kansen")
+    d30 = input ("In welke maand ben ik geboren? \nA Juni \nB Juli\nC Augustus\nHier typen:")
+    if d30 == "A" or d30 == "a":
+        print ("\nNee, dat is ook niet goed, ik ben geboren in Juli" )
+        print("maar je weet wat ze zeggen, drie keer is scheepsrecht")
+        d31 = input ("hoeveel huisdieren heb ik?\nA 0\nB 1\nC 2\nHier typen: ")
+        if d31 == "A" or d31 == "a":
+            print ("Alweer niet goed, ik had wel wat beter van je verwacht")
+            print ("De laatste vraag, kom op je kan dit(kies niet C)")
+            d32 = input ("Waar ben ik geboren\nA Nederland \nB Weet ik niet \nC Duitsland\nHier typen:")
+            if d32 == "A" or d32 == "a":
+                print ("Helaas, je hebt alles fout")
+                print ("oja, ik ben geboren in Duitsland")
+        if d31 == "B"or d31 == "b":
+                print ("Correct, ik heb op dit moment één huisdier")
+                d38 = input ("Welk platform gebruik ik om te gamen?\nA XBOX\nB PLAYSTATION\nC PC\nHier typen:")
+                if d38 == "A"or d38 == "a":
+                    print ("Ik speel op dit moment op een Playstation 4")
+        if d31 == "C" or d31 == "c":
+            print("Ik heb één huisdier op dit moment")
+            d39 = input ("wat is mijn favoriete game genre?\nA MOBA\nB RPG\nC FPS\nHier typen:")
+            if d39 == "A" or d39 == "a":
+                print ("Mijn favoriete game genre is FPS,maar mijn favoriete game is wel een MOBA")
+    if d30 == "B" or d30 == "b":
+        print ("Dat klopt, ik ben inderdaad in Juli geboren")
+        d33 = input ("Wat is mijn lievelings eten?\nA Pizza\nB Lasagne\nC Ravioli\nHier typen:")
+        if d33 == "A" or d33 == "a":
+            print ("Mijn favoriete eten is lasagne, helaas")
+            d34 = input ("wat is mijn favoriete game genre?\nA MOBA\nB RPG\nC FPS\nHier typen:")
+            if d34 == "A" or d34 == "a":
+                print ("Mijn favoriete game genre is FPS,maar mijn favoriete game is wel een MOBA")
+                d35 = input ("Wat is mijn favoriete kleur?\nA Lila\nB Grijs\nC Oranje\nHier typen:")
+                if d35 == "A" or d35 == "a":
+                    print ("Mijn favoriete kleur is grijs, erg saai")
+        if d33 == "B"or d33 == "b":
+            print ("Mijn lievelings eten is inderdaad lasagne")
+            d40 = input ("In welk land ben ik geboren?\nA Nederland\nB Duitsland\nC België\nHier typen:")
+            if d40 == "A" or d40 == "a":
+                print ("Ik ben geboren in Duitsland")
+        if d33 == "C" or d33 == "c":
+            print("Mijn lievelings eten is Lasagne, maar ravioli vind ik ook erg lekker")
+            d41 = input ("Welke schoenmaat heb ik?\nA 41\nB 42\nC 43\nHier typen:")
+            if d41 == "A" or d41 == "a":
+                print ("Mijn schoenmaat is 43, helaas")
+    if d30 == "C" or d30 == "c":
+        print ("Helaas, ik ben geboren in Juli")
+        d36 = input ("Welke shoenmaat heb ik?\nA 41\nB 42\nC 43\nHier typen:")
+        if d36 == "A" or d36 == "a":
+            print ("ik heb schoenmaat 43")
+            d37 = input ("Hoe lang ben ik?\nA 1,70\nB 1,75\nC 1,80\nHier typen:")
+            if d37 == "A" or d37 == "a":
+                print ("Ik ben 1.75")
+            if d37 == "B" or d37 == "b":
+                print ("Ik ben inderdaad 1.75")
+            if d37 == "C" or d37 == "c":
+                print ("Ik ben 1.75")
+        if d36 == "B" or d36 == "b":
+            print("Mijn schoenmaat is 43")
+            d42 = input ("Watvoor merk telefoon gebruik ik?\nA Apple\nB Samsung\nC Huawei\nHier typen:")
+            if d42 == "A" or d42 == "a":
+                print ("Ik gebruik op dit moment een Samsung")
+        if d36 == "C" or d36 == "c":
+            print ("Ik heb inderdaad schoenmaat 38")
+            d43 = input ("In welk land ben ik geboren?\nA Nederland\nDuitsland\nBelgië\nHier typen:")
+            if d43 == "A" or d43 == "a":
+                print ("Ik ben in duitsland geboren")
+            if d43 == "B" or d43 == "b":
+                print ("Ik ben in duitsland geboren")
+            if d43 == "C" or d43 == "c":
+                print ("Ik ben in duitsland geboren")
+            
